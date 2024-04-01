51c6b83a3c73400d2d730882eeb7d5ca052966e0
Asim <jafriaasim321@gmail.com>
2024-04-01 18:12:18 +0530

    server update

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config(); // import PORT
+require('dotenv').config(); // import PORT number
 const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
